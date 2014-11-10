0560e87b97e96bd65c823eecc9a7245592bdc9ad
Roman A. Davis <fluffyribbit@gmail.com>
2014-11-11 01:47:12 +0800

    Giving up on recursive ruby/euler7 for now.

--- Ruby/euler7.rb
+++ Ruby/euler7.rb
@@ -21,6 +21,8 @@ while true
 end
 
 # But can we do it recursively?
+# None of the below code works.
+
 # This method would probably work if the stack was infinite, but alas, it is
 # not.
 def rec_prime? n, i=3
@@ -36,3 +38,17 @@ def euler7 n=3, count=1
 end
 
 # maybe a sieve?
+# Doesn't work, either, unfortunately.
+def prime_list i=3, list=[2]
+  return list if i > 500
+  list << i if rec_prime? i
+  i += 2
+  prime_list i + 2, list
+end
+
+
+def euler_7 count=prime_list.length, i=501, a=prime_list
+  return i if count >= 100
+  count += 1 if a.all? {|d| i % d != 0}
+  euler_7 count, i + 2, a
+end
