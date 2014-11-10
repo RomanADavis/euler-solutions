db856345ce0fc82869347c8b14ccf924c11a2cdb
Roman A. Davis <fluffyribbit@gmail.com>
2014-11-11 00:09:34 +0800

    searching for recursion on 7 in ruby

--- Ruby/euler7.rb
+++ Ruby/euler7.rb
@@ -21,3 +21,18 @@ while true
 end
 
 # But can we do it recursively?
+# This method would probably work if the stack was infinite, but alas, it is
+# not.
+def rec_prime? n, i=3
+  return true if i > Math.sqrt(n)
+  return false if n % i == 0
+  rec_prime? n, i + 2
+end
+
+def euler7 n=3, count=1
+  return n if count == 1e4 + 1
+  count += 1 if rec_prime? n
+  euler7 n + 2, count
+end
+
+# maybe a sieve?
