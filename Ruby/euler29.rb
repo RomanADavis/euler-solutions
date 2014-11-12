0f15e3e63e625ef5574faf3ae19e9fe448cf1df0
Roman A. Davis <fluffyribbit@gmail.com>
2014-11-12 22:00:33 +0800

    Added euler29.rb

---
+++ Ruby/euler29.rb
@@ -0,0 +1,11 @@
+# euler29.rb
+# easy peasy. Tad slow, though. And I'm sure there's a more elegant way to do
+# this.
+a = []
+(2..100).each do |i|
+  (2..100).each do |j|
+     a << (i ** j)
+  end
+  a = a.uniq
+end
+p a.uniq.length
